46156617de10017a075293b803f5fcabd0d6df89
Billy <billygun27@gmail.com>
2018-04-07 21:10:01 +0700

    try

--- public/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require __DIR__ . '/../config.php'; 
-require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../app/route/container.php';
-require __DIR__ . '/../app/route/api.php';
