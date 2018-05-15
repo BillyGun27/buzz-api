5f5d39766441bf02e611de3eb3df787d3570ba80
Billy <billygun27@gmail.com>
2018-05-15 14:56:18 +0700

    cors

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
- header("Access-Control-Allow-Origin: *");
- 
+ //header("Access-Control-Allow-Origin: *");
+
 require __DIR__ . '/config.php'; 
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/app/route/container.php';
